5a371f33b4667a3e9e452322063ae2111e5199dc
unknown <ykcai@uwaterloo.ca>
2019-09-24 09:06:31 -0400

    added nodemon

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,4 +1,6 @@
 import { GraphQLServer } from "graphql-yoga";
+import { Agent } from "https";
+import { getPackedSettings } from "http2";
 
 // Scalar Types - String, Boolean, Int, Float, ID (unqiue)
 
@@ -9,27 +11,63 @@ const typeDefs = `
         name: String!
         location: String!
         bio: String!
+        id: ID!
+        age: Int!
+        gpa: Float
+        employed: Boolean!
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        inStock: Boolean
     }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
+    title() {
+      return "Michael Cai";
+    },
+    price() {
+      return "123.02";
+    },
+    releaseYear() {
+      return null;
+    },
+    rating() {
+      return "3.55";
+    },
+    inStock() {
+      return false;
+    },
     hello() {
       return "This is my first query!";
     },
     name() {
       return "Michael Cai";
+    },
+    id() {
+      return "abc123";
+    },
+    age() {
+      return 22;
+    },
+    employed() {
+      return true;
+    },
+    gpa() {
+      return null;
     }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs: typeDefs,
-  resolvers: resolvers
+  typeDefs,
+  resolvers
 });
 
 server.start(serverProps => {
-  console.log(`Server is starting`);
+  console.log("Server is starting");
   console.info(serverProps);
 });
